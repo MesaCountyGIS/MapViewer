5259a5c5ee601754d2080039581d88cbb26ccd50
Ryan Davison <ryandavison11@gmail.com>
2016-11-29 11:17:57 -0700

    Add test for _compareMS function in coordinateCleaner.js

--- tests/unit/testCoordinateCleaner.js
+++ tests/unit/testCoordinateCleaner.js
@@ -8,19 +8,37 @@ define([
         name: 'Coordinate Cleaner test',
 
         'Test coordinate function for array': function() {
-            assert.typeOf(coordinateCleaner.cleanCoordinates("39,108"), "array", 'The return value of two coordinates should be an array');
+            assert.typeOf(coordinateCleaner.cleanCoordinates("39,108"),
+            "array",
+            'The return value of two coordinates should be an array');
         },
 
         'Test coordinate function for appropriate return value': function() {
-            assert.deepEqual(coordinateCleaner.cleanCoordinates("39, 108"), ["39", "108"], 'with input of 39, 108, receive output as array of ["39", "108"]');
+            assert.deepEqual(coordinateCleaner.cleanCoordinates("39, 108"),
+            ["39", "108"],
+            'with input of 39, 108, receive output as array of ["39", "108"]');
         },
 
         'Test coordinate function with a callback function': function() {
-            assert.deepEqual(coordinateCleaner.cleanCoordinates("39,108", callback), callback("39","108"), 'The return value is an array');
+            assert.deepEqual(coordinateCleaner.cleanCoordinates("39,108", callback),
+            callback("39","108"),
+            'The return value is an array');
         },
 
-        'Test compareMS function': function() {
-            assert.deepEqual(coordinateCleaner._compareMS("39 25", "108 25"), 0, 'The return value of a latitude and longitude value in degrees/minutes is number 0');
+        'Test _compareMS function with two valid degree/minute values': function() {
+            assert.deepEqual(coordinateCleaner._compareMS("39 25", "108 25"),
+            0,
+            'The return value of a latitude and longitude value in degrees/minutes is number 0');
+        },
+
+        'Test _compareMS function with an invalid degree/minute value': function() {
+            assert.deepEqual(coordinateCleaner._compareMS(["39", "61"], ["108", "25"]),
+            "<div class='alertmessageIP alertmessage'>Minute and Second entries" +
+                " must be 0-60. Please correct the following indicated entries." +
+                "</div><br><b>Lat: 39</b> <u style='color:red;'>61</u>" +
+                " <br><br><b>Lon: 108</b> <b>25</b> ",
+            'The return value of a latitude and longitude value in degrees/minutes ' +
+            'with an invalid value is an HTML error message');
         }
 
 
